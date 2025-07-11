cf7924a9190178584ddb8d81a76ea00f42c6f66b
John Hilts <johnhilts@live.com>
2025-07-11 09:20:19 +0000

    jfh/lib added join util function

--- utility/string.lisp
+++ utility/string.lisp
@@ -4,3 +4,7 @@
 (defun string-starts-with (search string)
   "Does the beginning of string match the search string?"
   (search search string :start2 0 :end2 (length search)))
+
+(defun join (strings &optional delimiter)
+  (let ((format-string (if delimiter (format nil "~~{~~A~~^~A~~}" delimiter) "~{~A~}")))
+    (format nil format-string strings)))
